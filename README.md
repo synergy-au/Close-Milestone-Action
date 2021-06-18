3c472949e70a5fb8dae1daf2c6c75211ee112e20
Danijel Simic <danijel.simic@synergy.net.au>
2021-06-18 18:16:20 +0800

    Updated scripts to handle both pull request and issue closed events

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 This action is dedicated to help managing [GitHub Milestones](https://docs.github.com/en/issues/using-labels-and-milestones-to-track-work/about-milestones). It will check if there are any open issues or pull requests associated with the milestone, and if there are no open issues or pull requests left on the milestone, it will close the milestone.
 
-Currently this only works on a **pull request closed event** and not an issue closed event. However we'd love this action to support the issue closed event as well so pull requests are definitely welcome :smile:.
-
 ## Example GitHub Action Workflow File
 
 ```
@@ -11,6 +9,8 @@ name: Close Milestone
 
 on:
   pull_request:
+    types: [closed]
+  issues:
     types: [closed]
 
 jobs:
@@ -20,7 +20,17 @@ jobs:
     steps:
     - name: Run Close Milestone Action
       id: run-close-milestone-action
-      uses: synergy-au/Close-Milestone-Action@v1.0
+      uses: synergy-au/Close-Milestone-Action@v1.1
       with:
         secrets-token: ${{ secrets.GITHUB_TOKEN }}
 ```
+
+## Inputs
+
+**secrets-token**\
+The Secrets Github Token used for authenticating Github API calls.
+- required: true
+
+## Outputs
+
+None
--- close-milestone.sh
+++ close-milestone.sh
@@ -1,29 +1,61 @@
 #!/bin/bash
 
-# Initialize Shell Variables
+# Initialize variables
 MILESTONE_DATA=''
-PROCESSED_MILESTONE_DATA=''
 
-# Display Github Information
-echo Github Repository: "${REPOSITORY}"
-echo Pull Request Name: "${PULL_REQUEST_TITLE}"
-echo Pull Request Number: "${PULL_REQUEST_NUMBER}"
-
-# Check whether an OPEN milestone is linked to the pull request
-# If there is no milestone linked to the pull request then a value of "null" is returned from the API
-PULL_REQUEST_MILESTONE_DATA="${PULL_REQUEST_MILESTONE}"
-PULL_REQUEST_MILESTONE_STATE=$( echo "${PULL_REQUEST_MILESTONE}" | jq --raw-output .state )
-if [[ $PULL_REQUEST_MILESTONE != "null" && $PULL_REQUEST_MILESTONE_STATE == "open" ]];
+# Determine what the source is of the triggered event
+if [[ ${PULL_REQUEST_NUMBER} != "" ]];
 then
-    echo An open milestone was successfully found
-    MILESTONE_DATA=$PULL_REQUEST_MILESTONE_DATA
+    EVENT_TRIGGER_SOURCE='Pull Request'
+elif [[ ${ISSUE_NUMBER} != "" ]];
+then
+    EVENT_TRIGGER_SOURCE='Issue'
 else
-    echo There is no open milestone linked to the pull request
+    EVENT_TRIGGER_SOURCE='Unknown'
+fi
+echo Event Trigger Source: "$EVENT_TRIGGER_SOURCE"
+
+# Display information from Github and initialize variables based on the source of the triggered event
+echo Github Repository: "${REPOSITORY}"
+if [[ $EVENT_TRIGGER_SOURCE == "Pull Request" ]];
+then
+    echo Pull Request Name: "${PULL_REQUEST_TITLE}"
+    echo Pull Request Number: "${PULL_REQUEST_NUMBER}"
+
+    PULL_REQUEST_MILESTONE_DATA="${PULL_REQUEST_MILESTONE}"
+    PULL_REQUEST_MILESTONE_STATE=$( echo "${PULL_REQUEST_MILESTONE}" | jq --raw-output .state )
+
+    # Check whether an OPEN milestone is linked to the pull request
+    # If there is no milestone linked to the pull request then a value of "null" is returned from the API
+    if [[ $PULL_REQUEST_MILESTONE != "null" && $PULL_REQUEST_MILESTONE_STATE == "open" ]];
+    then
+        echo An open milestone was successfully found
+        MILESTONE_DATA=$PULL_REQUEST_MILESTONE_DATA
+    else
+        echo There is no open milestone linked to the pull request
+    fi
+elif [[ $EVENT_TRIGGER_SOURCE == "Issue" ]];
+then
+    echo Issue Name: "${ISSUE_TITLE}"
+    echo Issue Number: "${ISSUE_NUMBER}"
+
+    ISSUE_MILESTONE_DATA="${ISSUE_MILESTONE}"
+    ISSUE_MILESTONE_STATE=$( echo "${ISSUE_MILESTONE}" | jq --raw-output .state )
+
+    # Check whether an OPEN milestone is linked to the issue
+    # If there is no milestone linked to the issue then a value of "null" is returned from the API
+    if [[ $ISSUE_MILESTONE != "null" && $ISSUE_MILESTONE_STATE == "open" ]];
+    then
+        echo An open milestone was successfully found
+        MILESTONE_DATA=$ISSUE_MILESTONE_DATA
+    else
+        echo There is no open milestone linked to the issue
+    fi
 fi
 
 if [[ $MILESTONE_DATA != '' ]];
 then
-    # Retrieve only a specific set of data from the OPEN milestone linked to the pull request
+    # Retrieve only a specific set of data from the OPEN milestone linked to the issue
     OPEN_MILESTONE_DATA=$( echo "$MILESTONE_DATA" | jq --raw-output '{ title, number, open_issues, closed_issues, due_on }' )
     OPEN_ISSUES=$( echo "$OPEN_MILESTONE_DATA" | jq --raw-output .open_issues )
 
@@ -31,7 +63,7 @@ then
     if [[ $OPEN_ISSUES == 0 ]];
     then
         echo There are no open issues remaining on the milestone
-        PROCESSED_MILESTONE_DATA=$OPEN_MILESTONE_DATA
+        MILESTONE_DATA=$OPEN_MILESTONE_DATA
     else
         # Only for the purpose of making the message grammatically correct when read
         if [[ $OPEN_ISSUES == 1 ]];
@@ -41,18 +73,21 @@ then
             OPEN_ISSUES_TEXT=$( echo open issues )
         fi
         echo Milestone still has $OPEN_ISSUES $OPEN_ISSUES_TEXT linked to it and therefore cannot be closed yet
+
+        # Reset MILESTONE_DATA variable given the milestone is not eligible for closure and therefore should not be processed any further
+        MILESTONE_DATA=''
     fi
 fi
 
-if [[ $PROCESSED_MILESTONE_DATA != '' ]];
+if [[ $MILESTONE_DATA != '' ]];
 then
-    # Close the OPEN milestone if there are no open issues remaining
-    OPEN_MILESTONE=$( echo "$PROCESSED_MILESTONE_DATA" )
+    OPEN_MILESTONE=$( echo "$MILESTONE_DATA" )
     NUMBER=$( echo "$OPEN_MILESTONE" | jq --raw-output '.number' )
     TITLE=$( echo "$OPEN_MILESTONE" | jq --raw-output '.title' )
     CLOSED_ISSUES=$( echo "$OPEN_MILESTONE" | jq --raw-output '.closed_issues' )
     DUE_ON=$( echo "$OPEN_MILESTONE" | jq --raw-output '.due_on' )
 
+    # Close the OPEN milestone as there are no open issues remaining
     # Need to authenticate to obtain write access for the REST API PATCH event
     CLOSED_MILESTONE=$( curl --silent -X PATCH -H "Authorization: token ${SECRETS_TOKEN}" "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"${REPOSITORY}"/milestones/"$NUMBER" -d '{ "state":"closed" }' )
 
